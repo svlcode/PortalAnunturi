74c0898e152619b12a4d93a442c2060e85c86e9e
CareCarr-Netrom <carecarr@netrom.ro>
2018-06-25 14:26:51 +0300

    Initial commit

---
+++ src/PortalAnunturi/PortalAnunturi/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PortalAnunturi.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
